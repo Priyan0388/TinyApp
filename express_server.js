55cb7f6f69ec1d2b903756ec3f95d1c654fd84da
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-05 03:14:57 +0000

    added URL submission form, installed body-parser, added Post Route to receiver Form submission & created function for Random ShortURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,13 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    return Math.random().toString(36).substr(6) ;
+  }
+
 app.set('view engine', 'ejs');
 
 var urlDatabase = {
@@ -24,12 +31,21 @@ app.get("/urls.json", (req, res) => {
   app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+  });
 
-});
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     console.log(templateVars)
     res.render("urls_show", templateVars);
+  });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
   });
   
 app.listen(PORT, () => {
