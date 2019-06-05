27d139a959d5cf9194feae0b4c911278afbc04ca
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-05 19:24:21 +0000

    added redirect to longURL

--- express_server.js
+++ express_server.js
@@ -39,22 +39,22 @@ app.get("/urls.json", (req, res) => {
 
   app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-    console.log(templateVars)
     res.render("urls_show", templateVars);
   });
 
   app.get("/u/:shortURL", (req, res) => {
-    const longURL = {shortURL: urlDatabase[req.params.shortURL]};
-    res.send("longURL");
+    const longURL = urlDatabase[req.params.shortURL];
+    console.log(longURL)
+    res.redirect(longURL);
   });
-
+  
   app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
+    //console.log(req.body);  // Log the POST request body to the console
     const shortURL = generateRandomString();
     urlDatabase[shortURL] = req.body.longURL
     res.redirect("/urls/" + shortURL);         // Respond with 'Ok' (we will replace this)
   });
-  
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
