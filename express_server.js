59cbb5e8c66b9c65503c0afbddf37dec66b5363d
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-05 03:25:29 +0000

    created redirections for shortURLS & after form submission

--- express_server.js
+++ express_server.js
@@ -13,7 +13,8 @@ app.set('view engine', 'ejs');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  shortURL: longURL
 };
 
 app.get("/", (req, res) => {
@@ -43,9 +44,14 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_show", templateVars);
   });
 
+  app.get("/u/:shortURL", (req, res) => {
+    const longURL = {shortURL: urlDatabase[req.params.shortURL]};
+    res.redirect(longURL);
+  });
+
   app.post("/urls", (req, res) => {
     console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+    res.send("/u/:shortURL");         // Respond with 'Ok' (we will replace this)
   });
   
 app.listen(PORT, () => {
